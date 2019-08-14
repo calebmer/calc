d6083480566449d1ab3488fc3875935118075061
Caleb Meredith <calebmeredith8@gmail.com>
2019-08-13 19:05:32 -0700

    Add caching for calculations.

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,36 @@ export class Value<T> {
   }
 }
 
+const enum CalculationState {
+  Empty,
+  Normal,
+  Abrupt,
+}
+
 export class Calculation<T> {
   readonly _calculate: () => T;
+  _state = CalculationState.Empty;
+  _value: unknown = null;
 
   constructor(calculate: () => T) {
     this._calculate = calculate;
   }
 
   get(): T {
-    return this._calculate();
+    if (this._state === CalculationState.Empty) {
+      try {
+        this._value = this._calculate();
+        this._state = CalculationState.Normal;
+      } catch (value) {
+        this._value = value;
+        this._state = CalculationState.Abrupt;
+      }
+    }
+
+    if (this._state === CalculationState.Normal) {
+      return this._value as T;
+    } else {
+      throw this._value;
+    }
   }
 }
