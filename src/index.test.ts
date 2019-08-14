f662d41f5875572f1c09cca7e31a20ddd7bcf14f
Caleb Meredith <calebmeredith8@gmail.com>
2019-08-13 19:02:31 -0700

    Add the most basic functionality for calculations.

---
+++ src/index.test.ts
@@ -0,0 +1,77 @@
+import {Value, Calculation} from './index';
+
+test('can manually change a basic value', () => {
+  const value = new Value(1);
+
+  expect(value.get()).toEqual(1);
+  value.set(2);
+  expect(value.get()).toEqual(2);
+});
+
+test('will get the result of a calculation', () => {
+  const calculation1 = new Calculation(() => 1);
+  const calculation2 = new Calculation(() => 2);
+
+  expect(calculation1.get()).toEqual(1);
+  expect(calculation2.get()).toEqual(2);
+});
+
+test('will get the result of a throwing calculation', () => {
+  const error = new Error('test');
+  const calculation = new Calculation(() => {
+    throw error;
+  });
+
+  expect(() => calculation.get()).toThrow(error);
+});
+
+test('will get the result of a calculation multiple times', () => {
+  const calculation = new Calculation(() => 42);
+
+  expect(calculation.get()).toEqual(42);
+  expect(calculation.get()).toEqual(42);
+  expect(calculation.get()).toEqual(42);
+});
+
+test('will get the result of a throwing calculation multiple times', () => {
+  const error = new Error('test');
+  const calculation = new Calculation(() => {
+    throw error;
+  });
+
+  expect(() => calculation.get()).toThrow(error);
+  expect(() => calculation.get()).toThrow(error);
+  expect(() => calculation.get()).toThrow(error);
+});
+
+test('will only run a calculation once', () => {
+  const calculate = jest.fn(() => 42);
+  const calculation = new Calculation(calculate);
+
+  expect(calculation.get()).toEqual(42);
+  expect(calculation.get()).toEqual(42);
+  expect(calculation.get()).toEqual(42);
+  expect(calculate).toHaveBeenCalledTimes(1);
+});
+
+test('will only run a throwing calculation once', () => {
+  const error = new Error('test');
+  const calculate = jest.fn(() => {
+    throw error;
+  });
+  const calculation = new Calculation(calculate);
+
+  expect(() => calculation.get()).toThrow(error);
+  expect(() => calculation.get()).toThrow(error);
+  expect(() => calculation.get()).toThrow(error);
+  expect(calculate).toHaveBeenCalledTimes(1);
+});
+
+test('will run a calculation lazily', () => {
+  const calculate = jest.fn(() => 42);
+  const calculation = new Calculation(calculate);
+
+  expect(calculate).toHaveBeenCalledTimes(0);
+  expect(calculation.get()).toEqual(42);
+  expect(calculate).toHaveBeenCalledTimes(1);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,147 +1,28 @@
-let dependencyTracker: Set<Dependency> | null = null;
-
-function trackDependency(dependency: Dependency): void {
-  if (dependencyTracker === null) {
-    throw new Error('TODO: Message');
-  }
-  dependencyTracker.add(dependency);
-}
-
-class Dependency {
-  readonly _listeners = new Set<() => void>();
-
-  _addListener(listener: () => void): void {
-    this._listeners.add(listener);
-  }
-
-  _removeListener(listener: () => void): void {
-    this._listeners.delete(listener);
-  }
-}
-
-class Value<T> extends Dependency {
+export class Value<T> {
   _value: T;
 
   constructor(value: T) {
-    super();
     this._value = value;
   }
 
-  get() {
-    trackDependency(this);
+  get(): T {
     return this._value;
+  }
+
+  set(value: T): void {
+    // TODO: Schedule this for later?
+    this._value = value;
   }
 }
 
-const enum CalculationState {
-  Invalid,
-  Calculating,
-  Normal,
-  Abrupt,
-}
-
-class Calculation<T> extends Dependency {
+export class Calculation<T> {
   readonly _calculate: () => T;
-  _state = CalculationState.Invalid;
-  _value: unknown = null;
-  _dependencies: Set<Dependency> | null = null;
 
   constructor(calculate: () => T) {
-    super();
     this._calculate = calculate;
-    this._invalidate = this._invalidate.bind(this);
   }
 
   get(): T {
-    trackDependency(this);
-
-    if (this._state === CalculationState.Invalid) {
-      let lastDependencyTracker = dependencyTracker;
-      dependencyTracker = new Set();
-
-      this._state = CalculationState.Calculating;
-      try {
-        this._value = this._calculate();
-        this._state = CalculationState.Normal;
-      } catch (value) {
-        this._value = value;
-        this._state = CalculationState.Abrupt;
-      }
-
-      const dependencies = dependencyTracker;
-      dependencyTracker = lastDependencyTracker;
-
-      const lastDependencies = this._dependencies;
-      this._dependencies = dependencies;
-
-      if (this._listeners.size !== 0) {
-        dependencies.forEach(dependency => {
-          if (lastDependencies !== null) {
-            if (lastDependencies.delete(dependency)) {
-              return;
-            }
-          }
-          dependency._addListener(this._invalidate);
-        });
-
-        if (lastDependencies !== null) {
-          lastDependencies.forEach(lastDependency => {
-            lastDependency._removeListener(this._invalidate);
-          });
-        }
-      }
-    }
-
-    if (this._state === CalculationState.Calculating) {
-      throw new Error('TODO: Recursion');
-    }
-
-    if (this._state === CalculationState.Normal) {
-      return this._value as T;
-    } else {
-      throw this._value;
-    }
-  }
-
-  _addListener(listener: () => void): void {
-    this._listeners.add(listener);
-
-    if (this._listeners.size === 1 && this._dependencies !== null) {
-      this._dependencies.forEach(dependency => {
-        dependency._addListener(this._invalidate);
-      });
-    }
-  }
-
-  _removeListener(listener: () => void): void {
-    this._listeners.delete(listener);
-
-    if (this._listeners.size === 0 && this._dependencies !== null) {
-      this._dependencies.forEach(dependency => {
-        dependency._removeListener(this._invalidate);
-      });
-    }
-  }
-
-  _invalidate(): void {
-    if (this._state !== CalculationState.Invalid) {
-      this._state = CalculationState.Invalid;
-      this._value = null;
-      if (this._listeners.size === 0) {
-        this._dependencies = null;
-      } else {
-        this._listeners.forEach(callListener);
-      }
-    }
-  }
-}
-
-function callListener(listener: () => void): void {
-  try {
-    listener();
-  } catch (error) {
-    setTimeout(() => {
-      throw error;
-    }, 0);
+    return this._calculate();
   }
 }
