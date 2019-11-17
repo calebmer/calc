f5e726e6484f260e48705b5824be2221a06a0f3b
Caleb Meredith <calebmeredith8@gmail.com>
2019-11-17 13:56:17 -0800

    Add documentation for formula fields.

--- workspaces/calc/src/Formula.ts
+++ workspaces/calc/src/Formula.ts
@@ -2,11 +2,66 @@ import {Calc} from './Calc';
 import {objectIs} from './objectIs';
 
 export class Formula<T> extends Calc<T> {
+  /**
+   * Is the current formula value valid? If false then we will always
+   * recalculate the value. The formula will be in an invalid state when:
+   *
+   * - We initialize for the first time and have never calculated the value.
+   * - We are listening to changes in our dependencies and will invalidate when
+   *   a change occurs.
+   *
+   * While our formula is invalid we will never call listeners.
+   *
+   * If we never have listeners we will never enter an invalid state since we
+   * are not subscribed to our own dependencies. That means just because our
+   * formula is in a valid state does not mean our value is up-to-date.
+   *
+   * If the formula value is in a valid state then this property will be an
+   * integer. The integer represents which transaction we validated the formula
+   * value in. If we try to calculate the same formula twice in the same formula
+   * transaction we can reuse work since calculations are guaranteed to be
+   * immutable in a formula transaction.
+   */
   _valid: number | false = false;
+
+  /**
+   * What is the current version of our formula value? Notably, current does not
+   * mean latest. For every dependency we will store the current version of that
+   * dependency when we evaluated.
+   *
+   * The version will not change if we recalculate and the value is the exact
+   * same as determined by `Object.is`.
+   */
   _version: number = 0;
+
+  /**
+   * Did the calculation return normally or did it throw? When we return the
+   * value we will use this to decide whether or not to throw.
+   */
   _completion: FormulaCompletion = FormulaCompletion.Normal;
+
+  /**
+   * The current value calculated by our formula. May be stale if dependencies
+   * changed and we were not notified. We throw this value instead of returning
+   * if `_completion` is set to `FormulaCompletion.Abrupt`.
+   *
+   * When the formula is invalid (`this._valid === false`) we set this to `null`
+   * since we know the value is stale. That way the old value may be
+   * garbage collected.
+   */
   _value: unknown = null;
-  _dependencies: FormulaDependencies | null = null;
+
+  /**
+   * What are the dependencies of our formula? For every dependency we remember
+   * the version we used when calculating. That way when we are deciding whether
+   * we need to recalculate we can compare the latest version to the one that
+   * we used.
+   */
+  _dependencies: Map<Calc<unknown>, number> | null = null;
+
+  /**
+   * The user provided formula function which actually performs the calculation.
+   */
   readonly _calculate: () => T;
 
   constructor(calculate: () => T) {
@@ -192,6 +247,7 @@ export class Formula<T> extends Calc<T> {
   _callListeners(): void {
     if (this._valid !== false) {
       this._valid = false;
+      this._value = null; // Allow the value to be garbage collected.
       super._callListeners();
     }
   }
@@ -202,14 +258,12 @@ const enum FormulaCompletion {
   Abrupt = 'abrupt',
 }
 
-type FormulaDependencies = Map<Calc<unknown>, number>;
-
 /**
  * When evaluating a formula we capture its dependencies in this set. Every time
  * we start evaluating a formula we create a new set. Formula dependencies are
  * local to a single formula evaluation!
  */
-export let currentFormulaDependencies: FormulaDependencies | null = null;
+export let currentFormulaDependencies: Map<Calc<unknown>, number> | null = null;
 
 /**
  * When we evaluate a formula we record which transaction it was evaluated in.
