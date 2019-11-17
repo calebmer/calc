b08aa6b6e6094e7d574c4ccc13c09b3c2036dc4b
Caleb Meredith <calebmeredith8@gmail.com>
2019-11-17 14:22:44 -0800

    Add documentation to `Calc`.

--- workspaces/calc/src/Calc.ts
+++ workspaces/calc/src/Calc.ts
@@ -1,3 +1,14 @@
+/**
+ * Some reactive value which may change over time. You may listen for changes
+ * in the value. There are a few notable subclasses:
+ *
+ * - `Cell`: A cell is a reactive value with no dependencies that you may
+ *   change. While a `Calc` is immutable a `Cell` allows you to introduce
+ *   mutability into the system.
+ * - `Formula`: Some reactive value which is computed from other `Calc`
+ *   dependencies. The formula value will change when one of its
+ *   `Calc` dependencies changes.
+ */
 export abstract class Calc<T> {
   /**
    * Some of the dependents of this calculation. A dependent only registers
@@ -17,12 +28,37 @@ export abstract class Calc<T> {
    */
   _listeners: Set<() => void> | null = null;
 
+  /**
+   * Calculates the current value and records it as a dependency. Since we
+   * record this `Calc` as a dependency we will listen to changes.
+   *
+   * NOTE: You may not call this function outside of a reactive context! The
+   * function will throw outside of a reactive context since it can’t register
+   * itself as a dependency.
+   */
   abstract calc(): T;
 
+  /**
+   * Gets the current value of our `Calc` without listening to changes! Use this
+   * if you only need the current value for some one-off process. Otherwise use
+   * `calc()` which will record that you depend on this value and will listen
+   * to changes.
+   */
   abstract getWithoutListening(): T;
 
+  /**
+   * Gets the latest version for our `Calc`. If the value in the `Calc` is stale
+   * (maybe dependencies have changed) this will recalculate the value.
+   */
   abstract _getLatestVersion(): number;
 
+  /**
+   * Adds a dependent `Calc`. That way when this value updates we can notify
+   * all the `Calc`s that depend on us.
+   *
+   * A dependent only registers itself if it has a listener. Therefore for all
+   * dependents the following is true `_listeners !== null`.
+   */
   _addDependent(dependent: Calc<unknown>): void {
     if (this._dependents === null) {
       this._dependents = new Set();
@@ -30,6 +66,9 @@ export abstract class Calc<T> {
     this._dependents.add(dependent);
   }
 
+  /**
+   * Removes a dependent `Calc`.
+   */
   _removeDependent(dependent: Calc<unknown>): void {
     if (this._dependents !== null) {
       this._dependents.delete(dependent);
@@ -39,6 +78,18 @@ export abstract class Calc<T> {
     }
   }
 
+  /**
+   * Adds a user supplied listener function which may be called whenever a
+   * `Calc`’s value changes.
+   *
+   * The listener will be called whenever the latest value you requested is
+   * invalidated. If we’ve already notified you about an invalid value the
+   * listener may not be called again. Use `getWithoutListening()` when your
+   * listener is called to make sure you have the latest value.
+   *
+   * If the listener throws we will throw an unhandled exception in a different
+   * event loop context.
+   */
   addListener(listener: () => void): void {
     if (this._listeners === null) {
       this._listeners = new Set();
@@ -46,6 +97,9 @@ export abstract class Calc<T> {
     this._listeners.add(listener);
   }
 
+  /**
+   * Removes a user supplied listener.
+   */
   removeListener(listener: () => void): void {
     if (this._listeners !== null) {
       this._listeners.delete(listener);
